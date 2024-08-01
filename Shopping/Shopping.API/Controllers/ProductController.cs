2cb0610d9af414f7bb8d9693ed12081272d50905
rasul <rasul@RASUL>
2024-08-01 17:33:12 +0400

    first commit

---
+++ Shopping/Shopping.API/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using Shopping.API.Models;
+using Shopping.Client.Data;
+
+namespace Shopping.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        ProductContext _productContext;
+
+        public ProductController(ProductContext productContext)
+        {
+            _productContext = productContext;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return _productContext.Products.Find(x=>true).ToList();
+        }
+    }
+}
---
+++ Shopping/Shopping.Client/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using Shopping.Client.Data;
+using Shopping.Client.Models;
+using System.Diagnostics;
+
+namespace Shopping.Client.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public readonly HttpClient _httpClient;
+
+        public HomeController(ILogger<HomeController> logger, IHttpClientFactory httpClientFactory)
+        {
+            _logger = logger;
+            _httpClient = httpClientFactory.CreateClient("ShoppingAPIClient");
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var response = await _httpClient.GetFromJsonAsync<IEnumerable<Product>>("Product");
+            return View(response);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
